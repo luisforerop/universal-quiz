20a8981aa0c786bae4d81b8876878f4276319aea
Luis <Luis@LAPTOP-0EFHT9GS>
2023-02-19 21:46:02 -0500

    fix: :bug: Fix bug when it tries to get the baseUrl.

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -1,10 +1,5 @@
 export const endpoints = [
   {
-    account: 'qa',
-    urlBase: '',
-    host: '',
-  },
-  {
     account: 'prod',
     urlBase: 'https://n586ggoxy8.execute-api.us-east-1.amazonaws.com/dev',
     host: 'github',
@@ -19,7 +14,7 @@ export const endpoints = [
 export const endpoint = () => {
   const { host } = window.location
   return (
-    endpoints.find((endpoint) => endpoint.host.includes(host))?.urlBase ??
+    endpoints.find((endpoint) => host.includes(endpoint.host))?.urlBase ??
     'http://localhost:4000/dev'
   )
 }
